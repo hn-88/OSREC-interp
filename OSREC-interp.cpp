8a4eceb24465a798e7eecd5750d6ce9d28552829
hn-88 <hn-88@users.noreply.github.com>
2024-06-04 16:10:30 +0530

    add the first openfilename dialog

--- OSREC-interp.cpp
+++ OSREC-interp.cpp
@@ -45,49 +45,24 @@ using namespace cv;
 
 
 int main(int argc,char *argv[])
-{	
-	Mat m(960,1280,CV_16U);
-	randn(m,10000, 500);
-	Scalar M,D;
-	meanStdDev(m,M,D);
-	std::cout << M(0) << " " << D(0) << std::endl;
-	Mat m2 = m.reshape(0,1);
-	meanStdDev(m2,M,D);
-	std::cout << M(0) << " " << D(0);
-		
-	std::cout << std::endl << "The above was for randn(m,10000,500)" << std::endl;
-	std::cout << "Now testing for 1,000,000 with the bug report code from asitjain" << std::endl;
-	std::cout << "a dataset comprising approximately one million 1x1024D vectors. The dataset is normalized within the 0-255 range ..." << std::endl;
-
-	// generate the data
-	Mat u8DataMat(1024,1000000,CV_8U);
-	randn(u8DataMat,128, 10);
-	std::cout << "randn(u8DataMat,128, 10); --> generated data has a mean of 128 and stddev of 10" << std::endl;	
-
-	// Calculate the mean
-	cv::Scalar mean = cv::mean(u8DataMat);
+{
+	std::cout << "This tool takes an initial osrectxt file, appends to it camera keyframes from subsequent osrectxt files with suitable interpolation, and saves as a new osrectxt file.";
+	std::cout << std::endl << std::endl;
+	std::cout << "https://github.com/OpenSpace/OpenSpace/discussions/3294" << std::endl;
+	std::cout << "https://github.com/hn-88/openspace-scripts/wiki" << std::endl;
+	std::cout << "https://docs.openspaceproject.com/en/releases-v0.20.0/content/session-recording.html#ascii-file-format" << std::endl;
+	std::cout << std::endl;
 	
-	// Calculate the standard deviation
-	cv::Mat stdmat = u8DataMat - mean[0];
-	cv::Mat sqmat;
-	cv::multiply(stdmat, stdmat, sqmat);
-	
-	// Sum the squared differences
-	cv::Scalar sum = cv::sum(sqmat);
-	double dSum = sum[0];
-	double stdDev = dSum / (static_cast<double>(u8DataMat.rows) * u8DataMat.cols);
-	stdDev = sqrt(stdDev);
-	
-	std::cout << "Mean: " << mean[0] << " StdDev: " << stdDev << std::endl;
-	std::cout << "The above mean and std dev were as calculated using cv::sum, using static_cast<double> etc." << std::endl;
-
-	meanStdDev(u8DataMat,M,D);
-	std::cout << M(0) << " " << D(0) << " This is using meanStdDev(u8DataMat,M,D);" << std::endl;
-	Mat fdm2 = u8DataMat.reshape(0,1);
-	meanStdDev(fdm2,M,D);
-	std::cout << M(0) << " " << D(0) << " This is using meanStdDev(u8DataMat.reshape(0,1),M,D);" << std::endl;
-	
-    return 0;
+	char const * SaveFileName = "";
+	char const * OpenFileName = "";
+	char const * FilterPatterns[2] =  { "*.osrec","*.osrectxt" };
+	OpenFileName = tinyfd_openFileDialog(
+				"Open the initial osrectxt file",
+				"",
+				2,
+				FilterPatternsini,
+				NULL,
+				0);
 	   
 	   
 } // end main
